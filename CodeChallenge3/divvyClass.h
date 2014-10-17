ab8747a498d1e78c74ffebed982ae866d1ccd29b
Wade Sellers <Waderoni@gmail.com>
2014-10-17 10:26:09 -0500

    first commit

---
+++ CodeChallenge3/divvyClass.h
@@ -0,0 +1,13 @@
+//
+//  divvyClass.h
+//  CodeChallenge3
+//
+//  Created by Wade Sellers on 10/17/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface divvyClass : NSObject
+
+@end
---
+++ CodeChallenge3/divvyClass.m
@@ -0,0 +1,13 @@
+//
+//  divvyClass.m
+//  CodeChallenge3
+//
+//  Created by Wade Sellers on 10/17/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "divvyClass.h"
+
+@implementation divvyClass
+
+@end
